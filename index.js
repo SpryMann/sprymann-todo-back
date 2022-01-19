245ba22d9d2e6a492c47811f056bace608484707
SpryMann <lyakhovgm@gmail.com>
2022-01-19 20:23:51 +0300

    New commit

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://sprymann-todo-application-frnt.herokuapp.com/",
+    optionsSuccessStatus: 200,
+  })
+);
 app.use("/api", router);
 
 db.sequelize.sync(
