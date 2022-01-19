178aba34dc4acadb6ecc7980ca9dd301939aefa7
SpryMann <lyakhovgm@gmail.com>
2022-01-19 16:14:23 +0300

    New commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./models");
 const router = require("./routes");
-const Tasks = db.tasks;
+const Role = db.role;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,7 +12,13 @@ app.use(express.json());
 app.use(cors());
 app.use("/api", router);
 
-db.sequelize.sync();
+db.sequelize.sync(
+  Role.findOne({ where: { id: 1 } }).then((role) => {
+    if (!role) {
+      Role.create({ title: "USER" });
+    }
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server started at localhost:${port}`);
